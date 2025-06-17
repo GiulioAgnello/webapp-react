eb08dce73970dfe02a2d90777bb94918af1be2b3
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-17 16:44:52 +0200

    components_props

--- components/MovieList.jsx
+++ components/MovieList.jsx
@@ -1,10 +1,56 @@
 import Card from "./card";
 
+// example for movie array
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    release_year: 2010,
+    abstract:
+      "A skilled thief is given a chance at redemption if he can successfully perform inception.",
+    image: "inception.jpg",
+  },
+  {
+    id: 2,
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    release_year: 2010,
+    abstract:
+      "A skilled thief is given a chance at redemption if he can successfully perform inception.",
+    image: "inception.jpg",
+  },
+  {
+    id: 3,
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    release_year: 2010,
+    abstract:
+      "A skilled thief is given a chance at redemption if he can successfully perform inception.",
+    image: "inception.jpg",
+  },
+  {
+    id: 4,
+    title: "Inception",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+    release_year: 2010,
+    abstract:
+      "A skilled thief is given a chance at redemption if he can successfully perform inception.",
+    image: "inception.jpg",
+  },
+];
+
 export default function MovieList() {
   return (
     <>
-      <div className="row">
-        <Card />
+      <div className="row g-3">
+        {movies.map((movie) => (
+          <Card key={movie.id} movie={movie} />
+        ))}
       </div>
     </>
   );
--- components/card.jsx
+++ components/card.jsx
@@ -1,15 +1,26 @@
-export default function Card() {
+import { Link } from "react-router";
+
+export default function Card({ movie }) {
   return (
     <>
       <div className="col-4">
         <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={movie.image} className="card-img-top" alt={movie.title} />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick</p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+            <h5 className="card-title">{movie.title}</h5>
+            <p>
+              <small>{movie.director}</small>
+            </p>
+            <p>
+              <small>{movie.genre}</small>
+            </p>
+            <p>
+              <small>{movie.release_year}</small>
+            </p>
+            <p className="card-text">{movie.abstract}</p>
+            <Link href="#" className="btn btn-primary">
+              See more
+            </Link>
           </div>
         </div>
       </div>
