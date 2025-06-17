cab4fa9ae414f6b8f363aa983ee3885aff467168
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-17 18:08:03 +0200

    reviewsSection

--- components/ReviewCard.jsx
+++ components/ReviewCard.jsx
@@ -1,9 +1,9 @@
 export default function ReviewCard({ review }) {
   return (
-    <div className="d-flex py-2 justify-content-between border-bottom gap-2">
-      <p>avatar</p>
-      <p>{review.name}</p>
-      <p>{review.text}</p>
+    <div className="d-flex py-2 justify-content-between align-items-center border-bottom gap-2">
+      <p className="avatar">U</p>
+      <p className="card-username px-2">{review.name}</p>
+      <p className="flex-grow-1">{review.text}</p>
       <p>{review.vote}</p>
     </div>
   );
