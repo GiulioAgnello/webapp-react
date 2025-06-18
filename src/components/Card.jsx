a4ad9a4f50d9f669f1bdcf843b948944063f7e61
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-18 18:31:36 +0200

    colShow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,25 +7,26 @@ export default function Card({ movie }) {
 
   return (
     <>
-      <div className="col-4">
-        <div className="card ">
+      <div className="col-12 col-sm-4 col-xl-3">
+        <div className="card  shadow p-3 mb-5 bg-body-tertiary rounded">
           <img
             src={`${imagePath}${image}`}
-            className="card-img-top"
+            className="imageList"
             alt={movie.title}
           />
-          <div className="card-body">
+          <div className="card-body ">
             <h5 className="card-title">{title}</h5>
             <p>
-              <small>{director}</small>
+              {" "}
+              Director :<small> {director}</small>
             </p>
             <p>
-              <small>{genre}</small>
+              Genre :<small> {genre}</small>
             </p>
             <p>
-              <small>{release_year}</small>
+              Release :<small> {release_year}</small>
             </p>
-            <p className="card-text">{abstract}</p>
+            Abstract :<p className="card-text"> {abstract}</p>
             <Link to={`/movies/${id}`} className="btn btn-primary">
               See more
             </Link>
